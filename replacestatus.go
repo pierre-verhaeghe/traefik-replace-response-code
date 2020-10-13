38841b26fa27389f9e2faab6a447e62c7c29ece9
Pierre VERHAEGHE <pierre.verhaeghe@gmail.com>
2020-10-13 16:42:00 +0200

    first implementation, basic replacing

---
+++ replacestatus.go
@@ -0,0 +1,50 @@
+package traefik_replace_response_code
+
+import (
+	"context"
+	"net/http"
+)
+
+type responseWriterWithStatusCode struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// Config the plugin configuration.
+type Config struct {
+	inputCode  int `json:"inputCode,omitempty"`
+	outputCode int `json:"outputCode,omitempty"`
+}
+
+func CreateConfig() *Config {
+	return &Config{
+		inputCode:  429,
+		outputCode: 200,
+	}
+}
+
+type Limiter struct {
+	next       http.Handler
+	inputCode  int
+	outputCode int
+	name       string
+}
+
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	return &Limiter{
+		inputCode:  config.inputCode,
+		outputCode: config.outputCode,
+		next:       next,
+		name:       name,
+	}, nil
+}
+
+func (a *Limiter) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
+	responseWriter := responseWriterWithStatusCode{rw, 200}
+	a.next.ServeHTTP(responseWriter, req)
+
+	if responseWriter.statusCode == a.inputCode {
+		responseWriter.WriteHeader(a.outputCode)
+	}
+}
